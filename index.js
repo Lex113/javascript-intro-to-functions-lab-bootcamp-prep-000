39ee12882fd542f7cc5f3f938b0898e52fcbf3ec
Alexis Morton <alexismorton56@gmail.com>
2017-10-07 22:20:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ function sayHiToGrandma(string){
     var lowercase = string.toLowerCase()
     var uppercase = string.toUpperCase()
     if (string === lowercase){
-    return "I can\'t hear you."
+    return "I can\'t hear you!"
   } else if (string === uppercase){
-    return "YES, INDEED!"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma."){
-    return "I love you too."
+    return "I love you, too."
   } else {
     return "Grandma is not interested."
   }
